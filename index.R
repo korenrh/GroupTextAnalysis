c371f248735ed459d7ff43ddcff6d2df9f94324f
Reilly Koren <49700126+korenrh@users.noreply.github.com>
2021-10-03 22:35:15 -0500

    Updating index

--- index.R
+++ index.R
@@ -10,107 +10,132 @@ library("syuzhet")
 library("ggplot2")
 library("dplyr")
 
-## If you don't know what a package is, you can ask. Use ctrl-return or press Run above.
-## I have placed these queries in the code, but better practice is to type it directly
-## in the console below.
-
 ??syuzhet
 ??qdap
 
-## make VERY sure that the following packages have loaded-- base, datasets, dplyr, ggplot2, graphics, grDevices, methods, plyr, qdap, qdapDictionaries, qdapRegex, qdapTools, RColorBrewer, RCurl, RDSTK, readr, rjson, stats, syuzhet, twitteR, utils
+## loading in rap, pop, rock, and poetry
+drake = read_file("archive/drake.txt")
+kanye = read_file("archive/kanye-west.txt")
+biggie = read_file("archive/notorious-big.txt")
+mj = read_file("archive/michael-jackson.txt")
+britney = read_file("archive/britney-spears.txt")
+biebs = read_file("archive/bieber.txt")
+hendrix = read_file("archive/jimi-hendrix.txt")
+nirvana = read_file("archive/nirvana.txt")
+blink = read_file("archive/blink-182.txt")
+dickinson = read_file("archive/dickinson.txt")
+nursery_rhymes = read_file("archive/nursery_rhymes.txt")
+seuss = read_file("archive/dr-seuss.txt")
 
-gandhi_speech = read_file("assets/gandhi_speech2.txt")
-View(gandhi_speech)
+## collect all of our texts into a vector
+texts <- c(drake,kanye,biggie,mj,britney,biebs,hendrix,nirvana,blink,dickinson,nursery_rhymes,seuss)
 
-## loading President Obama's 2009 speech
-obama_speech = read_file("assets/obama_2009.txt")
-View(obama_speech)
-
-## polarity and sentiment
-g_scores = get_nrc_sentiment(gandhi_speech)
-o_scores = get_nrc_sentiment(obama_speech)
-class(o_scores)
-class(g_scores)
-
-g_polarity = g_scores[1,9:10]
-g_sentiment = g_scores[1,1:8]
-o_polarity = o_scores[1,9:10]
-o_sentiment = o_scores[1,1:8]
-
-## visualize polarity
-class(g_polarity)
-class(o_polarity)
-g_polarity = data.matrix(g_polarity, rownames.force = TRUE)
-o_polarity = data.matrix(o_polarity, rownames.force = TRUE)
-barplot(g_polarity)
-barplot(o_polarity)
-
-## visualize sentiment
-## Given that Obama was taking office on the heels of the recession
-## I would expect more positive sentiment, as his campaign
-## offered hope during a very difficult time in America
-class(g_sentiment)
-g_sentiment = data.matrix(g_sentiment, rownames.force = TRUE)
-barplot(g_sentiment)
-o_sentiment = data.matrix(o_sentiment,rownames.force = TRUE)
-barplot(o_sentiment)
-
-## break it down by sentence
-g_speech_sen = get_sentences(gandhi_speech)
-
-o_speech_sen = get_sentences(obama_speech)
-
-sentiment_vector = get_sentiment(g_speech_sen, method = "syuzhet")
-sentiment_vector_o = get_sentiment(o_speech_sen, method = "syuzhet")
-
-## A vector is a basic data structure in R.
-## It is a sequence of elements that share the same data type. 
-## A vector supports logical, integer, double, character, complex, or raw data type.
-## They have these in C++ too!
-
-sentiment_vector
-summary(sentiment_vector)
-boxplot(sentiment_vector)
-
-## let's see Obama's sentiments
-sentiment_vector_o
-summary(sentiment_vector_o)
-boxplot(sentiment_vector_o)
-
-#Result: generally pretty positive!
-
-## what was the most positive sentence in the paragraph?
-max(sentiment_vector)
-sentence_sentiment = data.frame(g_speech_sen, sentiment_vector)
-View(sentence_sentiment)
-which.max(sentence_sentiment$sentiment_vector)
-
-## we now have an index - what can we do with this?
-most_positive = sentence_sentiment[which.max(sentence_sentiment$sentiment_vector),]
-## don't forget the comma!
-
-## how about we try most negative as well...
-## let's see what Obama's most 'negative' sentence
-min(sentiment_vector_o)
-sentence_sentiment_o = data.frame(o_speech_sen, sentiment_vector_o)
-View(sentence_sentiment_o)
-most_negative = sentence_sentiment_o[which.min(sentence_sentiment_o$sentiment_vector_o),]
-## let's print the most negative
-print(most_negative)
-
-## Wordclouds
-## look over to the right and see that these packages are checked
-## if one is absent then go to the console below and type install.packages("MISSINGPACKAGE")
-library("devtools")
-library("RColorBrewer")
-library("tm")
-library("SnowballC")
-library("wordcloud")
+## get sentiment for each of these texts
+## collecting them all into a data frame
+## generating a barplot for each as well
+sentiment_data = data.frame()
+artists <- c("Drake","Kanye","B.I.G","MJ","Britney","Biebs","Hendrix","Nirvana","Blink-182","Dickinson","Suess","Nursery Rhymes")
+## looping over all 12 artist
+for(i in 1:12) {
+  ## get sentiment and polarity
+  df <- get_nrc_sentiment(texts[i])
+  sentiment_data <- rbind(sentiment_data, df)
+  polarity = df[1,9:10]
+  sentiment = df[1,1:8]
+  polarity = data.matrix(polarity, rownames.force = TRUE)
+  sentiment = data.matrix(sentiment, rownames.force = TRUE)
+  ## generate barplots for each
+  barplot(sentiment,
+          main = artists[i],
+          ylab = "Count",
+          names.arg = c("Anger","Anticipation","Disgust","Fear","Joy","Sadness","Surprise","Trust"),
+          las=2)
+  barplot(polarity,main=artists[i])
+}
+## view the sentiment counts as a table
+View(sentiment_data)
 
 
-wordcloud(gandhi_speech,colors=c("blue","orange"))
-## limiting to 50 words that must have been used twice
-## let's make a word cloud for Obama!
-wordcloud(obama_speech,colors=brewer.pal(8,"Dark2"),max.words=50,min.freq = 2,
-          random.order=FALSE,rot.per = 0.35)
+## Next, we need to read our files by line to treat the
+## lyric lines as sentences.
+## With this, we can do sentiment analysis by line!
+
+drake = readLines("archive/drake.txt")
+kanye = readLines("archive/kanye-west.txt")
+biggie = readLines("archive/notorious-big.txt")
+mj = readLines("archive/michael-jackson.txt")
+britney = readLines("archive/britney-spears.txt")
+biebs = readLines("archive/bieber.txt")
+hendrix = readLines("archive/jimi-hendrix.txt")
+nirvana = readLines("archive/nirvana.txt")
+blink = readLines("archive/blink-182.txt")
+dickinson = readLines("archive/dickinson.txt")
+nursery_rhymes = readLines("archive/nursery_rhymes.txt")
+seuss = readLines("archive/dr-seuss.txt")
+
+## collect all of our texts back into a vector
+
+## This could be done with a loop like with the sentiment word data
+## but R is a bit finicky with vectors within vectors, so we'll do this manually.
+## Here we'll get sentiment analysis for each, including mean, min, max, and
+## a boxplot!
+
+drake_sen = get_sentiment(drake, method = "syuzhet")
+kanye_sen = get_sentiment(kanye, method = "syuzhet")
+biggie_sen = get_sentiment(biggie, method = "syuzhet")
+boxplot(biggie_sen,kanye_sen,drake_sen,
+        main="Hip-Hop Sentiments",
+        names=c("Biggie","Kanye","Drake"))
+
+mj_sen = get_sentiment(mj, method = "syuzhet")
+britney_sen = get_sentiment(britney, method = "syuzhet")
+biebs_sen = get_sentiment(biebs, method = "syuzhet")
+boxplot(mj_sen,britney_sen,biebs_sen,
+        main="Pop Sentiments",
+        names=c("M. Jackson","B. Spears","J. Bieber"))
+
+hendrix_sen = get_sentiment(hendrix, method = "syuzhet")
+nirvana_sen = get_sentiment(nirvana, method = "syuzhet")
+blink_sen = get_sentiment(blink, method = "syuzhet")
+boxplot(hendrix_sen,nirvana_sen,blink_sen,
+        main="Alt/Rock Sentiments",
+        names=c("Hendrix","Nirvana","Blink-182"))
+
+dick_sen = get_sentiment(dickinson, method = "syuzhet")
+nursery_rhymes_sen = get_sentiment(nursery_rhymes, method = "syuzhet")
+seuss_sen = get_sentiment(seuss, method = "syuzhet")
+boxplot(dick_sen,nursery_rhymes_sen,seuss_sen,
+        main="Poetry/Children's Sentiments",
+        names=c("Dickinson","Nursery Rhymes","Dr. Seuss"))
+
+## calculate mean, min, max, 25th percentile and 75th percentile
+mean <- c(mean(drake_sen),mean(kanye_sen),mean(biggie_sen),
+         mean(mj_sen),mean(britney_sen),mean(biebs_sen),
+         mean(hendrix_sen),mean(nirvana_sen),mean(blink_sen),
+         mean(dick_sen),mean(nursery_rhymes_sen),mean(seuss_sen))
+min <- c(min(drake_sen),min(kanye_sen),min(biggie_sen),
+         min(mj_sen),min(britney_sen),min(biebs_sen),
+         min(hendrix_sen),min(nirvana_sen),min(blink_sen),
+         min(dick_sen),min(nursery_rhymes_sen),min(seuss_sen))
+max <- c(max(drake_sen),max(kanye_sen),max(biggie_sen),
+         max(mj_sen),max(britney_sen),max(biebs_sen),
+         max(hendrix_sen),max(nirvana_sen),max(blink_sen),
+         max(dick_sen),max(nursery_rhymes_sen),max(seuss_sen))
+p25 <- c(quantile(drake_sen,0.25),quantile(kanye_sen,0.25),quantile(biggie_sen,0.25),
+         quantile(mj_sen,0.25),quantile(britney_sen,0.25),quantile(biebs_sen,0.25),
+         quantile(hendrix_sen,0.25),quantile(nirvana_sen,0.25),quantile(blink_sen,0.25),
+         quantile(dick_sen,0.25),quantile(nursery_rhymes_sen,0.25),quantile(seuss_sen,0.25))
+p75 <- c(quantile(drake_sen,0.75),quantile(kanye_sen,0.75),quantile(biggie_sen,0.75),
+         quantile(mj_sen,0.75),quantile(britney_sen,0.75),quantile(biebs_sen,0.75),
+         quantile(hendrix_sen,0.75),quantile(nirvana_sen,0.75),quantile(blink_sen,0.75),
+         quantile(dick_sen,0.75),quantile(nursery_rhymes_sen,0.75),quantile(seuss_sen,0.75))
+
+## collect stats into a data frame
+stats = data.frame(artists,mean,min,max,p25,p75)
+View(stats)
+
+
+
+
+
 
